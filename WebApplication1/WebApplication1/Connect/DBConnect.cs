adb1ba696782d203afe750ad18e828f9d2defbb4
林萬庭 <tf625585@gmail.com>
2020-04-21 23:47:47 +0800

    DB連線、執行

---
+++ WebApplication1/WebApplication1/Connect/DBConnect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web;
+
+namespace WebApplication1.Connect
+{
+    public class DBConnect
+    {
+        public int DBExec(string cmdstr)
+        {
+            var i = 0;
+            var conStr = ConfigurationSettings.AppSettings["Tdb"].ToString();
+
+            using (var conn = new SqlConnection(conStr))
+            {
+                using (var cmd = new SqlCommand(cmdstr))
+                {
+                    conn.Open();
+                    i = cmd.ExecuteNonQuery();
+                }
+            }
+            return i;
+        }
+    }
+}
