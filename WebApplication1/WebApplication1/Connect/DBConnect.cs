ad1f7d6985f2d9b9fc17331cab3ecdb786c50852
林萬庭 <tf625585@gmail.com>
2020-04-22 21:32:13 +0800

    select api

--- WebApplication1/WebApplication1/Connect/DBConnect.cs
+++ WebApplication1/WebApplication1/Connect/DBConnect.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Web;
 
@@ -7,11 +8,11 @@ namespace WebApplication1.Connect
 {
     public class DBConnect
     {
+        protected string conStr = ConfigurationSettings.AppSettings["Tdb"].ToString();
+
         public int DBExec(string cmdstr)
         {
             var i = 0;
-            var conStr = ConfigurationSettings.AppSettings["Tdb"].ToString();
-
             using (var conn = new SqlConnection(conStr))
             {
                 using (var cmd = new SqlCommand(cmdstr))
@@ -21,6 +22,18 @@ namespace WebApplication1.Connect
                 }
             }
             return i;
+        }
+
+        public DataTable SqlQueryDt(string cmdstr)
+        {
+            var ds = new DataSet();
+            using (var conn = new SqlConnection(conStr))
+            {
+                var da = new SqlDataAdapter(cmdstr, conn);
+                da.Fill(ds);
+            }
+
+            return ds.Tables.Count > 0 ? ds.Tables[0] : new DataTable();
         }
     }
 }
--- WebApplication1/WebApplication1/Controllers/SelectController.cs
+++ WebApplication1/WebApplication1/Controllers/SelectController.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Web.Http;
+using WebApplication1.Connect;
 
 namespace WebApplication1.Controllers
 {
@@ -18,7 +21,19 @@ namespace WebApplication1.Controllers
         // GET api/<controller>/5
         public string Get(string id)
         {
-            return "value";
+            var str_json = "";
+            DBConnect dbconn = new DBConnect();
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("SELECT * FROM USERDATE");
+            var dt = dbconn.SqlQueryDt(sb.ToString());
+            //將DataTable轉成JSON字串
+            if (dt.Rows.Count == 0)
+                str_json = "查無資料";
+            else
+                str_json = JsonConvert.SerializeObject(dt, Formatting.Indented);
+
+            return str_json;
+
         }
     }
 }
